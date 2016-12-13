e3f5a1c8cc7b430f240f78d5ae0111b32422b1be
kwijik <joebalalaika@ya.ru>
2016-12-13 15:44:13 +0100

    add GrilleNavale and Navire classes

--- src/batailleNavale/GrilleNavale.java
+++ src/batailleNavale/GrilleNavale.java
@@ -2,49 +2,124 @@ package batailleNavale;
 
 public class GrilleNavale {
 
-	private Navire[] navires; 
-	private int nbNavires; // количество кораблей
+	private Navire[] navires;  // массив кораблей
+	private int nbNavires; // количество кораблей ?? Какие корабли?
 	private int tailleGrille; //размер поля
-	private Coordonnee[] tirsRecus;
+	private Coordonnee[] tirsRecus; // координаты клеток куда стреляли
 	private int nbTirsRecus; // количество выстрелов
 	
 	public GrilleNavale(int taille, int[] taillesNavires) {
-	    
+		this(taille);
+		
 	}
+	
+	public GrilleNavale(int taille,  int nbNavires) {
+	    // возвращает пустое поле размера taille и он может вместить nbNavires
+	}
+	
 	public GrilleNavale(int taille) {
-		
+		this.tailleGrille = taille;
+		navires = new Navire[0];
+		nbNavires = 0;
+		tirsRecus = new Coordonnee[taille * taille];
+		nbTirsRecus = 0;
 	}
-	/*
+	
+	
+	
+	 
+	 public String toString(){
+		 String cell = "";
+		 for (int y = 0; y < tailleGrille; y++) {
+			for (int x = 0; x < tailleGrille; x++) {
+				boolean estNavire = false; 
+				boolean estTouche = false;
+				
+				Coordonnee c = new Coordonnee(x, y);
+				for(Navire n: navires){
+					if(n.contient(c)){
+						estNavire = true;
+						break;
+					}
+				}
+				for (int i = 0; i < nbTirsRecus; i++) {
+					if (c.equals(tirsRecus[i])){
+						estTouche = true;
+						break;
+					}
+				
+				}
+				if (estNavire && estTouche){
+					cell += "X ";
+				}
+				if (!estNavire && estTouche){
+					cell += "O ";
+				}
+				if (!estNavire && !estTouche){
+					cell += ". ";
+				}
+				if (estNavire && !estTouche){
+					cell += "# ";
+				}
+			}
+			cell += "\n";
+		}
+		 return cell;
+	 }
+		 /*
+  A B C D E F G H I J
+1 . . . . . . . . . .
+2 . . . # # # . . . .
+3 . . . . . . # X # .
+4 . . O . . . . O . .
+5 . . . . . # . . . .
+6 . . . O . # . . . .
+7 . # # . . # . . . .
+8 . . . . . # . . . .
+9 . . . . . . . O . .
+10 . . . . . . . . . .
+
+//точка - свободная клетка, в которую не стреляли; # - занятая кораблем, O - свободная клетка получившая выстрел, X - подбитая часть корабля
+ * 
+	}
 	public boolean ajouteNavire(Navire n) {
-		
+		// true если это возможно, false если уже есть корабль или
 	}
-	public void placementAuto(int[] taillesNavires) {
-		
+	public void placementAuto(int[] taillesNavires) { СПРОСИТЬ!!! Откуда взялся массив, как его сделать
+		// автоматически и случайно расставляет корабли
 	}
 	private boolean estDansGrille(Coordonnee c) {
-		
+		// true если с в this
 	}
 	private boolean estDansTirsRecus(Coordonnee c) {
 		
 	}
 	private boolean ajouteDansTirsRecus(Coordonnee c) {
-		
+		//прибавляет с к полученным выстреллам this если необходимо. 
+		 // возвращает true если this был изменен 
 	}
 	public boolean recoitTir(Coordonnee c) {
-		
+		// добавляет с к выстрелам полученным this если необходимо. Возвращает true
+		// если c не получала выстрелы и может задеть корабль this
 	}
 	public boolean estTouche(Coordonnee c) {
-		
+		// true если корабль this был подбит в c
 	}
 	public boolean estALEau(Coordonnee c) { // на воде
-		
+		// true если this стрельнул в с и попал в воду
 	}
 	
 	public boolean estCoule(Coordonnee c) { // подбит
-		
+		// true если корабль this был задет в c
 	}
 	public boolean perdu() { // потоплен
 		// this.perdu() -> true / false
 	}
 	*/
+	 public static void main(String[] args) {
+		GrilleNavale gn = new GrilleNavale(10);
+		System.out.println(gn);
+	}
 }
+
+//среда 8-30 (10-30 по Москве) 
--- src/batailleNavale/Navire.java
+++ src/batailleNavale/Navire.java
@@ -1,5 +1,7 @@
 package batailleNavale;
 
+import java.util.ArrayList;
+
 public class Navire {
 
 	private Coordonnee debut;
@@ -26,42 +28,170 @@ public class Navire {
 		return fin;
 	}
 
-	public boolean contient(Coordonnee c) { //проверка для горизонтального
+	public boolean contient(Coordonnee c) { // проверка для горизонтального
 		if (debut.getLigne() == fin.getLigne()) {
 			if (debut.getColonne() <= c.getColonne() && fin.getColonne() >= c.getColonne()) {
 				return true;
 			}
-		} else { //проверка для вертикального
+		} else { // проверка для вертикального
 			if (debut.getLigne() <= c.getLigne() && fin.getLigne() >= c.getLigne()) {
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-/*
+
 	public boolean touche(Navire n) {
-		
+		// true если this прилегает к n. По диагонали не считается за соседство
+		ArrayList<Coordonnee> navire1, navire2;
+		navire1 = new ArrayList<Coordonnee>();
+		navire2 = new ArrayList<Coordonnee>();
+		if (debut.getLigne() == fin.getLigne()) {
+			// идем по колонкам
+			for (int i = debut.getColonne(); i <= fin.getColonne(); i++) {
+				Coordonnee c = new Coordonnee(debut.getLigne(), i);
+				// System.out.println(c);
+				navire1.add(c);
+			}
+		} else {
+			for (int i = debut.getLigne(); i <= fin.getLigne(); i++) {
+				Coordonnee c = new Coordonnee(i, debut.getColonne());
+				// System.out.println(c);
+				navire1.add(c);
+			}
+		}
+		System.out.println();
+		if (n.debut.getLigne() == n.fin.getLigne()) {
+			// идем по колонкам
+			for (int i = n.debut.getColonne(); i <= n.fin.getColonne(); i++) {
+				Coordonnee c = new Coordonnee(n.debut.getLigne(), i);
+				// System.out.println(c);
+				navire2.add(c);
+			}
+		} else {
+
+			for (int i = n.debut.getLigne(); i <= n.fin.getLigne(); i++) {
+				Coordonnee c = new Coordonnee(i, n.debut.getColonne());
+				// System.out.println(c);
+				navire2.add(c);
+			}
+		}
+
+		for (Coordonnee a : navire1) {
+			for (Coordonnee b : navire2) {
+				if (a.voisine(b)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public boolean chevauche(Navire n) {
-		
+		// true если this и n занимают как минимум одну общую координату
+		ArrayList<Coordonnee> navire1, navire2;
+		navire1 = new ArrayList<Coordonnee>();
+		navire2 = new ArrayList<Coordonnee>();
+		if (debut.getLigne() == fin.getLigne()) {
+			// идем по колонкам
+			for (int i = debut.getColonne(); i <= fin.getColonne(); i++) {
+				Coordonnee c = new Coordonnee(debut.getLigne(), i);
+				// System.out.println(c);
+				navire1.add(c);
+			}
+		} else {
+			for (int i = debut.getLigne(); i <= fin.getLigne(); i++) {
+				Coordonnee c = new Coordonnee(i, debut.getColonne());
+				// System.out.println(c);
+				navire1.add(c);
+			}
+		}
+		System.out.println();
+		if (n.debut.getLigne() == n.fin.getLigne()) {
+			// идем по колонкам
+			for (int i = n.debut.getColonne(); i <= n.fin.getColonne(); i++) {
+				Coordonnee c = new Coordonnee(n.debut.getLigne(), i);
+				// System.out.println(c);
+				navire2.add(c);
+			}
+		} else {
+
+			for (int i = n.debut.getLigne(); i <= n.fin.getLigne(); i++) {
+				Coordonnee c = new Coordonnee(i, n.debut.getColonne());
+				// System.out.println(c);
+				navire2.add(c);
+			}
+		}
+
+		for (Coordonnee a : navire1) {
+			for (Coordonnee b : navire2) {
+				if (a.equals(b)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public boolean recoitTir(Coordonnee c) {
+		// true если this содержит
+		ArrayList<Coordonnee> navire1;
+		navire1 = new ArrayList<Coordonnee>();
+		if (debut.getLigne() == fin.getLigne()) {
+			// идем по колонкам
+			for (int i = debut.getColonne(); i <= fin.getColonne(); i++) {
+				Coordonnee v = new Coordonnee(debut.getLigne(), i);
+				// System.out.println(c);
+				navire1.add(v);
+			}
+		} else {
+			for (int i = debut.getLigne(); i <= fin.getLigne(); i++) {
+				Coordonnee v = new Coordonnee(i, debut.getColonne());
+				// System.out.println(c);
+				navire1.add(v);
+			}
+		}
+
+		for (Coordonnee a : navire1) {
+
+			if (a.equals(c)) {
+				partiesTouchees[nbTouchees] = a;
+				nbTouchees += 1; 
+				return true;
+			}
+		}
 		
+		return false;
+
 	}
 
-	public boolean estTouche(Coordonnee c) {
-		
+	
+	 public boolean estTouche(Coordonnee c) { 
+		 //true если this подбита на координате с 
+		 for (int i=0; i < nbTouchees; i++){
+			 if (c.equals(partiesTouchees[i])){
+				 return true;
+			 }
+		 }
+		 return false;
 	}
-
-	public boolean estTouche() {
-		
+	  
+	  public boolean estTouche() { //true если у this хоть одна клетка подбита
+		  return nbTouchees != 0;
+	  }
+	  
+	 public boolean estCoule() { 
+		 return partiesTouchees.length == nbTouchees;
+		 
+	 }
+	 
+	public static void main(String[] args) {
+		Navire n1 = new Navire(new Coordonnee(6, 7), 2, false);
+		Navire n2 = new Navire(new Coordonnee(7, 7), 3, false);
+		System.out.println(n1.touche(n2));
 	}
-
-	public boolean estCoule() {
-		
-	}
-*/
 }
+// 18-30 - 20-30
+// 15-15 - 16-45 вторник
+//
